84fd741b6e0047d9dea0d4eedc8a41407f8a7d89
AllenChinese <13402202685@163.com>
2019-03-25 23:18:25 +0800

    init: setting webpack

---
+++ webpack.base.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './app/src/index.js',
+  output: {
+    filename: 'bundle.[hash].js',
+    path: path.join(__dirname, '/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './app/src/index.html',
+    }),
+    new CleanWebpackPlugin()
+  ]
+}
