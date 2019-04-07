1f09a4986d741795d8f533258756541c864ca74e
AllenChinese <13402202685@163.com>
2019-04-08 00:10:40 +0800

    feat: messageWindow  part layout

--- app/src/views/chatRoom/components/messageWindow/messageWindow.js
+++ app/src/views/chatRoom/components/messageWindow/messageWindow.js
@@ -1,45 +1,111 @@
 import React, { Component } from 'react';
-import { Tag } from 'antd';
+import { Tag, Input } from 'antd';
 import './messageWindow.less';
 
-function singleSidebar() {
+function SingleSidebar(props) {
+  const sidebarMsg = props.sidebarMsg;
   return (
     <div className="single-sidebar">
       <div className="static-msg">
         <h2>项目信息</h2>
-        <p>项目名：Chat Service</p>
+        <p>项目名：{sidebarMsg.projectMsg.name}</p>
         <p>
           Github：
-          <a href="https://github.com/AllenChinese/Chat-Service">
-            Chat Service
+          <a href={sidebarMsg.projectMsg.githubAddress}>
+            {sidebarMsg.projectMsg.name}
           </a>
         </p>
       </div>
       <div className="static-msg">
         <h2>个人信息</h2>
-        <p>微信：zjw13402202685</p>
-        <p>公司：丁香园</p>
+        <p>微信：{sidebarMsg.personalMsg.wechat}</p>
+        <p>公司：{sidebarMsg.personalMsg.company}</p>
         <p>
-          内推：<a href="mailto:zjiawei@dxy.cn">zjiawei@dxy.cn</a>
+          内推：
+          <a href={'mailto:' + sidebarMsg.personalMsg.job}>
+            {sidebarMsg.personalMsg.job}
+          </a>
         </p>
       </div>
       <div className="static-msg">
         <h2>标签</h2>
-        <Tag color="#f50">Express</Tag>
-        <Tag color="#2db7f5">React</Tag>
-        <Tag color="#108ee9">Antd</Tag>
-        <Tag color="#87d068">Socket</Tag>
+        {sidebarMsg.tags.map(item => {
+          <Tag color={item.color}>{item.tag}</Tag>;
+        })}
       </div>
     </div>
   );
 }
 
+class SingleWindow extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { TextArea } = Input;
+    return (
+      <div className="single-window">
+        <div className="single-window-top">
+          <i className="iconfont iconshouji" />
+          <p>杭州滨江 Allen</p>
+        </div>
+        <div className="single-window-content" />
+        <div className="single-window-footer">
+          <div className="function-bar">
+            <i className="iconfont iconemoji" />
+            <i className="iconfont icontupian" />
+            <i className="iconfont iconlishixiaoxi" />
+          </div>
+          <div className="input-area">
+            <TextArea className="textarea" rows={4} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
 class MessageWindow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebarMsg: {
+        projectMsg: {
+          name: 'Chat Service',
+          githubAddress: 'https://github.com/AllenChinese/Chat-Service'
+        },
+        personalMsg: {
+          wechat: 'zjw13402202685',
+          company: '丁香园',
+          job: 'zjiawei@dxy.cn'
+        },
+        tags: [
+          {
+            color: '#f50',
+            tag: 'Express'
+          },
+          {
+            color: '#2db7f5',
+            tag: 'React'
+          },
+          {
+            color: '#108ee9',
+            tag: 'Antd'
+          },
+          {
+            color: '#87d068',
+            tag: 'Socket'
+          }
+        ]
+      }
+    };
+  }
   render() {
     return (
       <div className="message-window">
-        <div className="single-window">this is MessageWindow</div>
-        {singleSidebar()}
+        <SingleWindow />
+        <SingleSidebar sidebarMsg={this.state.sidebarMsg} />
       </div>
     );
   }
