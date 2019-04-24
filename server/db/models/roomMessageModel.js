9af031cb67ba891d0ed5750ae95429f1ba6c6a81
AllenChinese <13402202685@163.com>
2019-04-24 23:25:24 +0800

    fix: DB Collections 名字必须小写

--- server/db/models/roomMessageModel.js
+++ server/db/models/roomMessageModel.js
@@ -1,7 +1,7 @@
-/** UserModel.js */
+/** RoomMessage.js */
 const mongoose = require('mongoose')
 const RoomMessageSchema = require('../schemas/roomMessageSchema')
 
-/** 创建 model, user 对应 mongodb 数据库中 users 的 collections */
-const User = mongoose.model('roomMessage', RoomMessageSchema)
-module.exports = User
+/** 创建 model, roomMessage 对应 mongodb 数据库中 users 的 collections */
+const RoomMessage = mongoose.model('roommessages', RoomMessageSchema)
+module.exports = RoomMessage
